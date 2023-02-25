9ac9f4530d2d688e9c737b6052cc709b404008c4
Nick Pappin <nick@papp.in>
2023-02-24 22:10:30 -0800

    Added download function. Working.

--- builder.py
+++ builder.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
 
+import requests, json, os
+from tqdm import tqdm
+def download():
+    s = requests.Session()
+    s.headers.update({'User-Agent': 'bslTools'})
+    response = s.get("https://broadbandmap.fcc.gov/nbm/map/api/published/filing")
+    parsed = json.loads(response.text)
+    uuid = parsed['data'][0]['process_uuid']
+    url = f'https://broadbandmap.fcc.gov/nbm/map/api/national_map_process/nbm_get_data_download/{uuid}'
+    response = s.get(url)
+    parsed = json.loads(response.text)
+    dataToProcess = parsed['data']
+    dataToProcess = [item for item in dataToProcess if item['state_name'] != None]
+    dataToProcess = [item for item in dataToProcess if item['file_type'] == 'csv']
+    if not os.path.isdir('data'):
+        os.makedirs('data')
+
+    cachedFiles = os.listdir('data')
+    cachedFiles = [entry for entry in cachedFiles if entry.endswith(".zip")]
+    cachedFileNames = [x.split('.')[0] for x in cachedFiles]
+
+    for item in tqdm(dataToProcess):
+        # print(item)
+        if item['file_name'] not in cachedFileNames:
+            url = f"https://broadbandmap.fcc.gov/nbm/map/api/getNBMDataDownloadFile/{item['id']}/1"
+            r = s.get(url)
+            filename = f'{os.path.join("data", item["file_name"])}.zip'
+            open(filename, 'wb').write(r.content)
+    return True
 
 def main():
+    download()
     return True
 
 if __name__ == "__main__":
