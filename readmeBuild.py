b77420ee55a7f22b9a4e4394c3e44294c5b31d8e
Nick Pappin <nick@papp.in>
2023-03-13 12:59:24 -0700

    Quick tool for building readme links.

---
+++ readmeBuild.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+
+stateList = [f[0:2] for f in os.listdir('states')]
+
+url = 'https://pub-96372591292d4fdca85ff0f6db6c67c2.r2.dev/states'
+
+for state in stateList:
+    lookupStr = f'[CSV]({url}/{state}BslLookup.csv) [Parquet]({url}/{state}BslLookup.parquet)'
+    blockStr = f'[CSV]({url}/{state}BlockBsls.csv) [Parquet]({url}/{state}BlockBsls.parquet)'
+    h3Str = f'[CSV]({url}/{state}H3Bsls.csv) [Parquet]({url}/{state}H3Bsls.parquet)'
+    line = f'| {state.upper()} | {lookupStr} | {blockStr} | {h3Str} |'
+    print(line)
