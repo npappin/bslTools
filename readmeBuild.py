ae753dfa9bf5e2a3dde626805a2d810a80325d1a
Nick Pappin <nick@papp.in>
2023-03-14 12:02:10 -0700

    fixed url

--- readmeBuild.py
+++ readmeBuild.py
@@ -3,12 +3,14 @@
 import os
 
 stateList = [f[0:2] for f in os.listdir('states')]
+stateList = list(set(stateList))
+stateList.sort()
 
-url = 'https://pub-96372591292d4fdca85ff0f6db6c67c2.r2.dev/states'
+url = 'https://pub-96372591292d4fdca85ff0f6db6c67c2.r2.dev/bslTools/states'
 
 for state in stateList:
-    lookupStr = f'[CSV]({url}/{state}BslLookup.csv) [Parquet]({url}/{state}BslLookup.parquet)'
-    blockStr = f'[CSV]({url}/{state}BlockBsls.csv) [Parquet]({url}/{state}BlockBsls.parquet)'
-    h3Str = f'[CSV]({url}/{state}H3Bsls.csv) [Parquet]({url}/{state}H3Bsls.parquet)'
+    lookupStr = f'[CSV]({url}/{state}BslLookup.csv) / [Parquet]({url}/{state}BslLookup.parquet)'
+    blockStr = f'[CSV]({url}/{state}BlockBsls.csv) / [Parquet]({url}/{state}BlockBsls.parquet)'
+    h3Str = f'[CSV]({url}/{state}H3Bsls.csv) / [Parquet]({url}/{state}H3Bsls.parquet)'
     line = f'| {state.upper()} | {lookupStr} | {blockStr} | {h3Str} |'
     print(line)
